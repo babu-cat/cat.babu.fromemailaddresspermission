b9ed104138f12e5b7b2442a62684767e51947353
Francesc Bassas i Bullich <francesc@babu.cat>
2017-08-02 17:14:06 +0200

    CiviCRM 4.7.22 compatible

--- CRM/Core/BAO/Email.php
+++ CRM/Core/BAO/Email.php
@@ -2,44 +2,44 @@
 /*
  +--------------------------------------------------------------------+
  | CiviCRM version 4.7                                                |
-+--------------------------------------------------------------------+
- | Copyright CiviCRM LLC (c) 2004-2016                                |
-+--------------------------------------------------------------------+
-| This file is a part of CiviCRM.                                    |
-|                                                                    |
-| CiviCRM is free software; you can copy, modify, and distribute it  |
-| under the terms of the GNU Affero General Public License           |
-| Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
-|                                                                    |
-| CiviCRM is distributed in the hope that it will be useful, but     |
-| WITHOUT ANY WARRANTY; without even the implied warranty of         |
-| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
-| See the GNU Affero General Public License for more details.        |
-|                                                                    |
-| You should have received a copy of the GNU Affero General Public   |
-| License and the CiviCRM Licensing Exception along                  |
-| with this program; if not, contact CiviCRM LLC                     |
-| at info[AT]civicrm[DOT]org. If you have questions about the        |
-| GNU Affero General Public License or the licensing of CiviCRM,     |
-| see the CiviCRM license FAQ at http://civicrm.org/licensing        |
-+--------------------------------------------------------------------+
-*/
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2017                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+ */
 
 /**
  *
-* @package CRM
- * @copyright CiviCRM LLC (c) 2004-2016
-*/
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2017
+ */
 
 /**
  * This class contains functions for email handling.
-*/
+ */
 class CRM_Core_BAO_Email extends CRM_Core_DAO_Email {
-
+	
 	/**
-   * Create email address.
-   *
-   * Note that the create function calls 'add' but  has more business logic.
+	 * Create email address.
+	 *
+	 * Note that the create function calls 'add' but  has more business logic.
 	 *
 	 * @param array $params
 	 *   Input parameters.
@@ -51,12 +51,12 @@ class CRM_Core_BAO_Email extends CRM_Core_DAO_Email {
 		if (is_numeric(CRM_Utils_Array::value('is_primary', $params)) || empty($params['id'])) {
 			CRM_Core_BAO_Block::handlePrimary($params, get_class());
 		}
-
+		
 		$email = CRM_Core_BAO_Email::add($params);
-
+		
 		return $email;
 	}
-
+	
 	/**
 	 * Takes an associative array and adds email.
 	 *
@@ -69,22 +69,22 @@ class CRM_Core_BAO_Email extends CRM_Core_DAO_Email {
 	public static function add(&$params) {
 		$hook = empty($params['id']) ? 'create' : 'edit';
 		CRM_Utils_Hook::pre($hook, 'Email', CRM_Utils_Array::value('id', $params), $params);
-
+		
 		$email = new CRM_Core_DAO_Email();
 		$email->copyValues($params);
-    if (!empty($email->email)) {
-		// lower case email field to optimize queries
-		$strtolower = function_exists('mb_strtolower') ? 'mb_strtolower' : 'strtolower';
-		$email->email = $strtolower($email->email);
-    }
-
+		if (!empty($email->email)) {
+			// lower case email field to optimize queries
+			$strtolower = function_exists('mb_strtolower') ? 'mb_strtolower' : 'strtolower';
+			$email->email = $strtolower($email->email);
+		}
+		
 		/*
 		 * since we're setting bulkmail for 1 of this contact's emails, first reset all their other emails to is_bulkmail false
 		 *  We shouldn't not set the current email to false even though we
 		 *  are about to reset it to avoid contaminating the changelog if logging is enabled
 		 * (only 1 email address can have is_bulkmail = true)
 		 */
-    if ($email->is_bulkmail != 'null' && !empty($params['contact_id']) && !self::isMultipleBulkMail()) {
+		if ($email->is_bulkmail != 'null' && !empty($params['contact_id']) && !self::isMultipleBulkMail()) {
 			$sql = "
 			UPDATE civicrm_email
 			SET    is_bulkmail = 0
@@ -95,21 +95,21 @@ class CRM_Core_BAO_Email extends CRM_Core_DAO_Email {
 			}
 			CRM_Core_DAO::executeQuery($sql);
 		}
-
+		
 		// handle if email is on hold
 		self::holdEmail($email);
-
+		
 		$email->save();
-
+		
 		if ($email->is_primary) {
 			// update the UF user email if that has changed
 			CRM_Core_BAO_UFMatch::updateUFName($email->contact_id);
 		}
-
+		
 		CRM_Utils_Hook::post($hook, 'Email', $email->id, $email);
 		return $email;
 	}
-
+	
 	/**
 	 * Given the list of params in the params array, fetch the object
 	 * and store the values in the values array
@@ -117,12 +117,12 @@ class CRM_Core_BAO_Email extends CRM_Core_DAO_Email {
 	 * @param array $entityBlock
 	 *   Input parameters to find object.
 	 *
-	 * @return bool
+	 * @return array
 	 */
-	public static function &getValues($entityBlock) {
+	public static function getValues($entityBlock) {
 		return CRM_Core_BAO_Block::getValues('email', $entityBlock);
 	}
-
+	
 	/**
 	 * Get all the emails for a specified contact_id, with the primary email being first
 	 *
@@ -138,7 +138,7 @@ class CRM_Core_BAO_Email extends CRM_Core_DAO_Email {
 		if (!$id) {
 			return NULL;
 		}
-
+		
 		$query = "
 SELECT    email,
           civicrm_location_type.name as locationType,
@@ -157,7 +157,7 @@ ORDER BY  civicrm_email.is_primary DESC, email_id ASC ";
 						'Integer',
 				),
 		);
-
+		
 		$emails = $values = array();
 		$dao = CRM_Core_DAO::executeQuery($query, $params);
 		$count = 1;
@@ -170,7 +170,7 @@ ORDER BY  civicrm_email.is_primary DESC, email_id ASC ";
 					'email' => $dao->email,
 					'locationTypeId' => $dao->locationTypeId,
 			);
-
+			
 			if ($updateBlankLocInfo) {
 				$emails[$count++] = $values;
 			}
@@ -180,7 +180,7 @@ ORDER BY  civicrm_email.is_primary DESC, email_id ASC ";
 		}
 		return $emails;
 	}
-
+	
 	/**
 	 * Get all the emails for a specified location_block id, with the primary email being first
 	 *
@@ -195,10 +195,10 @@ ORDER BY  civicrm_email.is_primary DESC, email_id ASC ";
 		if (empty($entityElements)) {
 			return NULL;
 		}
-
+		
 		$entityId = $entityElements['entity_id'];
 		$entityTable = $entityElements['entity_table'];
-
+		
 		$sql = " SELECT email, ltype.name as locationType, e.is_primary as is_primary, e.on_hold as on_hold,e.id as email_id, e.location_type_id as locationTypeId
 		FROM civicrm_loc_block loc, civicrm_email e, civicrm_location_type ltype, {$entityTable} ev
 		WHERE ev.id = %1
@@ -206,14 +206,14 @@ ORDER BY  civicrm_email.is_primary DESC, email_id ASC ";
 		AND   e.id IN (loc.email_id, loc.email_2_id)
 		AND   ltype.id = e.location_type_id
 		ORDER BY e.is_primary DESC, email_id ASC ";
-
+		
 		$params = array(
 				1 => array(
 						$entityId,
 						'Integer',
 				),
 		);
-
+		
 		$emails = array();
 		$dao = CRM_Core_DAO::executeQuery($sql, $params);
 		while ($dao->fetch()) {
@@ -226,10 +226,10 @@ ORDER BY  civicrm_email.is_primary DESC, email_id ASC ";
 					'locationTypeId' => $dao->locationTypeId,
 			);
 		}
-
+		
 		return $emails;
 	}
-
+	
 	/**
 	 * Set / reset hold status for an email
 	 *
@@ -274,7 +274,7 @@ AND    reset_date IS NULL
 			}
 		}
 	}
-
+	
 	/**
 	 * Build From Email as the combination of all the email ids of the logged in user and
 	 * the domain email id
@@ -283,10 +283,9 @@ AND    reset_date IS NULL
 	 *   an array of email ids
 	 */
 	public static function getFromEmail() {
-		$session = CRM_Core_Session::singleton();
-		$contactID = $session->get('userID');
+		$contactID = CRM_Core_Session::singleton()->getLoggedInContactID();
 		$fromEmailValues = array();
-
+		
 		/**
 		 * FROM EMAIL ADDRESS PERMISSION
 		 */
@@ -294,35 +293,35 @@ AND    reset_date IS NULL
 		// +
 		if (CRM_Core_Permission::check('site from email address')) {
 		// +
-		
-		// add all configured FROM email addresses
-		$domainFrom = CRM_Core_OptionGroup::values('from_email_address');
-		foreach (array_keys($domainFrom) as $k) {
-			$domainEmail = $domainFrom[$k];
-			$fromEmailValues[$domainEmail] = htmlspecialchars($domainEmail);
-		}
-		
+			
+			// add all configured FROM email addresses
+			$domainFrom = CRM_Core_OptionGroup::values('from_email_address');
+			foreach (array_keys($domainFrom) as $k) {
+				$domainEmail = $domainFrom[$k];
+				$fromEmailValues[$domainEmail] = htmlspecialchars($domainEmail);
+			}
+			
 		/**
 		 * FROM EMAIL ADDRESS PERMISSION
 		 */
-		
+			
 		// +
 		}
-	    // +
-
+		// +
+		
 		// add logged in user's active email ids
 		if ($contactID) {
 			$contactEmails = self::allEmails($contactID);
 			$fromDisplayName = CRM_Core_DAO::getFieldValue('CRM_Contact_DAO_Contact', $contactID, 'display_name');
-
-			foreach ($contactEmails as $emailId => $emailVal) {
+			
+			foreach ($contactEmails as $emailVal) {
 				$email = trim($emailVal['email']);
 				if (!$email || $emailVal['on_hold']) {
 					continue;
 				}
 				$fromEmail = "$fromDisplayName <$email>";
 				$fromEmailHtml = htmlspecialchars($fromEmail) . ' ' . $emailVal['locationType'];
-
+				
 				if (!empty($emailVal['is_primary'])) {
 					$fromEmailHtml .= ' ' . ts('(preferred)');
 				}
@@ -331,23 +330,23 @@ AND    reset_date IS NULL
 		}
 		return $fromEmailValues;
 	}
-
+	
 	/**
 	 * @return object
 	 */
 	public static function isMultipleBulkMail() {
-    return Civi::settings()->get('civimail_multiple_bulk_emails');
+		return Civi::settings()->get('civimail_multiple_bulk_emails');
 	}
-
+	
 	/**
 	 * Call common delete function.
-   *
-   * @param int $id
-   *
-   * @return bool
+	 *
+	 * @param int $id
+	 *
+	 * @return bool
 	 */
 	public static function del($id) {
 		return CRM_Contact_BAO_Contact::deleteObjectWithPrimary('Email', $id);
 	}
-
+	
 }
--- README.md
+++ README.md
@@ -8,14 +8,17 @@ The permission that enables this functionality is 'site from email address'.
 
 **Important**
 
-This extension overrides the following core 4.7.12 CiviCRM files:
+This extension overrides the following core 4.7.22 CiviCRM files:
 
 - CRM/Contact/Form/Task/EmailCommon.php
-
 - CRM/Core/BAO/Email.php
 
 ### Road Map
 
 - Check all the core files where you can get from email address. Currently only has covered the most obvious. 
-
 - It would be desirable if there were no way a user can send mail from addresses that were not his. So would prevent the user could change email addresses of CiviCRM users.
+
+### Related work and discussion
+
+- [How to disable “From email address” for unauthorized users?](https://civicrm.stackexchange.com/questions/7236/how-to-disable-from-email-address-for-unauthorized-users)
+- [CRM-5491 - ACL or Drupal permission to allow certain roles to choose between the "From email addresses"] (https://issues.civicrm.org/jira/browse/CRM-5491)
